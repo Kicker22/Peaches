8687c51d7c3d7d714df2a055c804f0a36fc94ec8
davidbridges123 <david.bridges@live.com>
2019-06-26 13:36:35 -0600

    working firebase auth

--- assests/javascript/login.js
+++ assests/javascript/login.js
@@ -23,33 +23,53 @@ $( document ).ready(function() {
   loginBtn.on("click", function () {
 
     const email = emailTxt.val();
-    const pass = passTxt.val();
-    const auth = firebase.auth();
-
-    auth.signInWithEmailAndPassword(email, pass);
+    const password = passTxt.val();
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
   });
   // adding singup button listener
   signupBtn.on("click", function () {
     const email = emailTxt.val();
-    const pass = passTxt.val();
-    const auth = firebase.auth();
+    const password = passTxt.val();
 
-    auth.createUserWithEmailAndPassword(email, pass);
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        console.log(errorCode);
+        
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        // ...
+      });
 
   });
 
 
-  firebase.auth().onAuthStateChanged(firebaseUser => {
-    if (firebaseUser) {
-        console.log(firebaseUser)
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      // ...
     }
-    else {
-        console.log("not-signed-in!")
-    }
+  });
 
   });
 
 
 
   
-});
