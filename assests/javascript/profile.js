c793675f4fe78d71e9b594f02573dffea43f5f62
Matt <mattsully01@gmail.com>
2019-06-26 20:24:20 -0600

    pushing for david also set up recipe API

---
+++ assests/javascript/profile.js
@@ -0,0 +1,18 @@
+
+
+$(".submit-button").on('click' ,function(event){
+    event.preventDefault()
+    var search = $("#recipes").val()
+    console.log(search)
+    var queryURL= "https://api.edamam.com/search?q=" + search + "&app_id=ef658656&app_key=7459cb2e2c002db95d605b639d3344b7&from=0&to=1&calories=591-722&health=alcohol-free"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    
+    .then(function(response){
+        console.log(response.hits[0])
+    })
+
+})
