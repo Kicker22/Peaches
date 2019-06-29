068b6a5ee29569a3ea8fe22f337c1f6f8cc3dc53
Matt <mattsully01@gmail.com>
2019-06-28 21:48:55 -0600

    javascript changes

--- assests/javascript/profile.js
+++ assests/javascript/profile.js
@@ -2,7 +2,7 @@
 $(".submit-button").on('click' ,function(event){
     event.preventDefault()
     var search = $("#recipes").val()
-    console.log(search)
+    // console.log(search)
     var queryURL= "https://api.edamam.com/search?q=" + search + "&app_id=ef658656&app_key=7459cb2e2c002db95d605b639d3344b7&from=0&to=1&calories=591-722&health=alcohol-free"
 
     $.ajax({
@@ -11,7 +11,21 @@ $(".submit-button").on('click' ,function(event){
     })
     
     .then(function(response){
-        console.log(response.hits[0])
-    })
-
+        // console.log(response.hits[0]);
+        
+        var newDiv = $('<div>')
+        newDiv.addClass('recipe-list')
+        newDiv.text() = searchResponse[i].text
+        var searchResponse = response.hits[0].recipe.ingredients;
+        console.log(searchResponse[i].text)
+    });
 })
+
+
+
+        
+           
+
+
+
+
