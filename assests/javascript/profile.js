f5119ffafe2d4465f4346287aef677eb0ca91f05
davidbridges123 <david.bridges@live.com>
2019-07-01 21:16:06 -0600

    adding second api text grab

--- assests/javascript/profile.js
+++ assests/javascript/profile.js
@@ -1,4 +1,7 @@
 
+
+var inglist = [];
+
 $(".submit-button").on('click', function (event) {
     event.preventDefault()
     // this code adds a container for the for loop recipie item before the loop runs 
@@ -16,14 +19,20 @@ $(".submit-button").on('click', function (event) {
 
         .then(function (response) {
             var searchResponse = response.hits
-            console.log(searchResponse)
+            // console.log(searchResponse)
 
             // this block adds list items from object array
             // and displays them on individual lines inside of the listDiv 
             for (let i = 0; i < searchResponse.length; i++) {
-                var listDiv = $('<div>')
-                var p = $('<p>')
-
+                var listDiv = $('<div>');
+                var p = $('<p>');
+            //    // console.log(search);
+            //     console.log(response);
+            //     console.log(searchResponse);
+                var button = $("<button>");
+                button.attr("id", "testA")
+                button.addClass('nutritionBtn btn btn-lg btn-primary');
+                button.text('Get nutrition info');
 
                 listDiv.addClass('mainList')
                 listDiv.fadeOut(0)
@@ -32,6 +41,7 @@ $(".submit-button").on('click', function (event) {
                 
                 var recipeName = searchResponse[i].recipe.label
                 var recipe = searchResponse[i].recipe.ingredients
+                // $(inglist).push(recipe);
                 var recipeImg = searchResponse[i].recipe.image
                 var image = $("<img src='" + recipeImg + "'>")
                 image.addClass('recipeImg')
@@ -43,16 +53,25 @@ $(".submit-button").on('click', function (event) {
                 listDiv.append(image)
                 
                 var label = $('<ul>')
+                label.append(button);
                 $(listDiv).append(label)
+                // this .parent . find .li .text
+                
                 
                 for (let j = 0; j < recipe.length; j++) {
                     var newLi = $('<li>')
                     newLi.text(recipe[j].text)
                     label.append(newLi)
                     $(".recipeLabel").append(listDiv)
+
                 }
                 listDiv.slideDown(1000)
-                
+                        
+               // console.log(searchResponse[0].recipe.ingredientLines);
+                inglist.push(searchResponse[i].recipe.ingredientLines);
+
+
+
 
             }
             // var label = $('<div>')
@@ -61,7 +80,36 @@ $(".submit-button").on('click', function (event) {
             // $(listDiv).prepend(label)
 
         });
+
 })
+console.log(inglist);
+
+$("body").on("click", "#testA", function(event) {
+    event.preventDefault();
+    console.log("TESTEINGNIGNGNIG")
+    console.log($(".nutritionBtn").val());
+    
+    // // console.log(this);
+    // var recUrl = (searchResponse[0].recipe.uri);
+    // var nutURL = "https://api.edamam.com/api/nutrition-data?app_id=1e0daad1&app_key=5631297951d8d491e104a39d6c17a9f9&url=" + recUrl
+
+    
+
+    //  $.ajax({
+    //      url: nutURL,
+    //       method: "GET"
+    //   })
+ 
+    //    .then(function (response) {
+            
+            console.log($(this).parent().find('li').text());
+            
+     //   });
+ });
+
+
+
+
 
 
 
