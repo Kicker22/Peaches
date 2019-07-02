9e103e6e359206eaa6736443467066c228bb9e42
Nathalie Solis-Maravilla <nathaliesolis-maravilla@nathalies-mbp.uconnect.utah.edu>
2019-07-01 18:10:36 -0600

    calculator

--- assests/javascript/userdata.js
+++ assests/javascript/userdata.js
@@ -12,6 +12,23 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      window.location.replace("login.html");
+  
+    }
+  });
 
 
 
@@ -22,23 +39,47 @@ var edamam = "https://api.edamam.com/search?q=chicken&app_id=ef658656&app_key=74
 
 // Create questions
 var questions ={
-    
+   question: "What's your current weight?",
+   question1: "What's your level of excercise?", 
+   question2: "Would you like to lose weight or bulk up?", 
+   question3: "How frequently are you planning to work out?",
+   question4: "How much weight would you like to lose?",
+   question5: "What is your preferred diet?",
+   
+},
+ 
+
+// console.log(questions);
+
+function computeBMI() {
+  // user inputs
+  var height = Number(document.getElementById("height").value);
+  var heightunits = document.getElementById("heightunits").value;
+  var weight = Number(document.getElementById("weight").value);
+  var weightunits = document.getElementById("weightunits").value;
+
+
+  //Convert all units to metric
+  if (heightunits == "inches") height /= 39.3700787;
+  if (weightunits == "lb") weight /= 2.20462;
+
+  //Perform calculation
+
+  //        var BMI = weight /Math.pow(height, 2)*10000;
+  var BMI = Math.round(weight / Math.pow(height, 2) * 10000);
+
+  //Display result of calculation
+  document.getElementById("output").innerText = Math.round(BMI * 100) / 100;
+
+  var output = Math.round(BMI * 100) / 100
+  if (output < 18.5)
+      document.getElementById("comment").innerText = "Underweight";
+  else if (output >= 18.5 && output <= 25)
+      document.getElementById("comment").innerText = "Normal";
+  else if (output >= 25 && output <= 30)
+      document.getElementById("comment").innerText = "Obese";
+  else if (output > 30)
+      document.getElementById("comment").innerText = "Overweight";
+  // document.getElementById("answer").value = output; 
 }
 
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
-    var displayName = user.displayName;
-    var email = user.email;
-    var emailVerified = user.emailVerified;
-    var photoURL = user.photoURL;
-    var isAnonymous = user.isAnonymous;
-    var uid = user.uid;
-    var providerData = user.providerData;
-    // ...
-  } else {
-    // User is signed out.
-    window.location.replace("login.html");
-
-  }
-});
