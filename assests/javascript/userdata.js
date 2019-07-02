a029f663fb8a94bfeacbb81de308c5d2d88259be
Nathalie Solis-Maravilla <nathaliesolis-maravilla@nathalies-mbp.uconnect.utah.edu>
2019-07-01 21:01:50 -0600

    working bmi cal

--- assests/javascript/userdata.js
+++ assests/javascript/userdata.js
@@ -26,7 +26,7 @@ firebase.initializeApp(firebaseConfig);
 //     } else {
 //       // User is signed out.
 //       window.location.replace("login.html");
-  
+
 //     }
 //   });
 
@@ -34,22 +34,34 @@ firebase.initializeApp(firebaseConfig);
 
 
 
-var edamam = "https://api.edamam.com/search?q=chicken&app_id=ef658656&app_key=7459cb2e2c002db95d605b639d3344b7&from=0&to=3&calories=591-722&health=alcohol-free"
 
-var start = document.getElementById("#start-btn");
-start.addEventListener("click", start);
+// var bmi = document.getElementById("#BMI").val;
+
+function getbmivalue() {
+  var weight = parseFloat(document.getElementById("weight").value);
+  var height = parseFloat(document.getElementById("height").value);
+ height = height * 12;  
+ var newbmivalue = weight *703 / Math.pow(height,2); 
+ console.log(newbmivalue)
+ console.log(weight,height)
+
+ document.getElementById('bmivalue').value = newbmivalue
+
+}
+
+
 
 // Create questions
-var questions ={
-   question: "What's your current weight?",
-   question1: "What's your level of excercise?", 
-   question2: "Would you like to lose weight or bulk up?", 
-   question3: "How frequently are you planning to work out?",
-   question4: "How much weight would you like to lose?",
-   question5: "What is your preferred diet?",
-   
-},
- 
+// var questions ={
+//    question: "What's your current weight?",
+//    question1: "What's your level of excercise?", 
+//    question2: "Would you like to lose weight or bulk up?", 
+//    question3: "How frequently are you planning to work out?",
+//    question4: "How much weight would you like to lose?",
+//    question5: "What is your preferred diet?",
+
+// },
+
 
 // console.log(questions);
 
@@ -68,37 +80,37 @@ var questions ={
 
 
 //   BMI Calculator 
-function computeBMI() {
-  // user inputs
-  var height = Number(document.getElementById("height").value);
-  var heightunits = document.getElementById("heightunits").value;
-  var weight = Number(document.getElementById("weight").value);
-  var weightunits = document.getElementById("weightunits").value;
+// function computeBMI() {
+//   // user inputs
+//   var height = Number(document.getElementById("height").value);
+//   var heightunits = document.getElementById("heightunits").value;
+//   var weight = Number(document.getElementById("weight").value);
+//   var weightunits = document.getElementById("weightunits").value;
 
 
-  //Convert all units to metric
-  if (heightunits == "inches") height /= 39.3700787;
-  if (weightunits == "lb") weight /= 2.20462;
+//   //Convert all units to metric
+//   if (heightunits == "inches") height /= 39.3700787;
+//   if (weightunits == "lb") weight /= 2.20462;
 
-  //Perform calculation
+//   //Perform calculation
 
-        var BMI = weight /Math.pow(height, 2)*10000;
-  var BMI = Math.round(weight / Math.pow(height, 2) * 10000);
+//         var BMI = weight /Math.pow(height, 2)*10000;
+//   var BMI = Math.round(weight / Math.pow(height, 2) * 10000);
 
-  //Display result of calculation
-  document.getElementById("output").innerText = Math.round(BMI * 100) / 100;
+//   //Display result of calculation
+//   document.getElementById("output").innerText = Math.round(BMI * 100) / 100;
 
-  var output = Math.round(BMI * 100) / 100
-  if (output < 18.5)
-      document.getElementById("comment").innerText = "Underweight";
-  else if (output >= 18.5 && output <= 25)
-      document.getElementById("comment").innerText = "Normal";
-  else if (output >= 25 && output <= 30)
-      document.getElementById("comment").innerText = "Obese";
-  else if (output > 30)
-      document.getElementById("comment").innerText = "Overweight";
-   document.getElementById("answer").value = output; 
-}
+//   var output = Math.round(BMI * 100) / 100
+//   if (output < 18.5)
+//       document.getElementById("comment").innerText = "Underweight";
+//   else if (output >= 18.5 && output <= 25)
+//       document.getElementById("comment").innerText = "Normal";
+//   else if (output >= 25 && output <= 30)
+//       document.getElementById("comment").innerText = "Obese";
+//   else if (output > 30)
+//       document.getElementById("comment").innerText = "Overweight";
+//    document.getElementById("answer").value = output; 
+// }
 
-computeBMI()
+// computeBMI()
 
